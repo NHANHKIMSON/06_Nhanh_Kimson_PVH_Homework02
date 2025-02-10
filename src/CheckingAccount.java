fb5b85fb4db12917b00d7daf8ee39529ed215bc8
nhanhkimson.biu@gmail.com <nhanhkimson.biu@gmail.com>
2025-02-10 16:49:14 +0700

    deposit

---
+++ src/CheckingAccount.java
@@ -0,0 +1,107 @@
+public class CheckingAccount extends Account {
+
+    private String accountNumber;
+    private String userName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private double balance = 0;
+
+
+    @Override
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    void withdraw(double amount) {
+        if(balance >amount){
+            balance -= amount;
+        }else {
+            System.out.println("You don't have enough money to withdrew!:" + " $" + balance);
+        }
+    }
+
+    @Override
+    void transfer(double amount, Account account) {
+
+    }
+    @Override
+    void display() {
+        System.out.println(Main.Colors.YELLOW + "\n===== Account Information =====" + Main.Colors.RESET);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("User Name: " + userName);
+        System.out.println("Date of Birth: " + dateOfBirth);
+        System.out.println("Gender: " + gender);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Balance: $" + balance);
+        System.out.println(Main.Colors.YELLOW + "===============================" + Main.Colors.RESET);
+
+    }
+//    -----------------------------------------
+
+    public CheckingAccount(String accountNumber, String dateOfBirth, String gender, String phoneNumber, String userName) {
+        this.accountNumber = accountNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.userName = userName;
+    }
+
+
+//    -------------------------------------------
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+//    ------------------------------------------------
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,86 @@
+import org.nocrala.tools.texttablefmt.BorderStyle;
+import org.nocrala.tools.texttablefmt.CellStyle;
+import org.nocrala.tools.texttablefmt.ShownBorders;
+import org.nocrala.tools.texttablefmt.Table;
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
-
-    static int option = 0;
-
-
-
-
-
-
+    private static CheckingAccount checkingAccount;
+    private static SavingAccount savingAccount;
+    private static String accountNumber;
+    static String option;
 
 
 
     public static void main(String[] args) {
-        do{
-            dispalyMenu();
-            int option = scanner.nextInt();
-            scanner.nextLine();
-
+        while (true){
+            displayMenu();
+            option = scanner.nextLine();
             switch (option) {
-                case 1:
-//                    createAccount();
+                case "1":
+                    createAccount();
                     break;
-                case 2:
-//                    depositMoney();
+                case "2":
+                     depositMoney();
                     break;
-                case 3:
-//                    withdrawMoney();
+                case "3":
+//                     withdrawMoney();
                     break;
-                case 4:
-//                    transferMoney();
+                case "4":
+//                     transferMoney();
                     break;
-                case 5:
-//                    displayAccountInfo();
+                case "5":
+                    System.out.println(Main.Colors.YELLOW + "\n===== Account Information =====" + Main.Colors.RESET);
+                     displayAccountInfo();
                     break;
-                case 6:
-//                    deleteAccount();
+                case "6":
+//                     deleteAccount();
                     break;
-                case 7:
+                case "7":
                     System.out.println(Colors.BLUE + "Exiting the system... Goodbye!" + Colors.RESET);
                     System.exit(0);
                 default:
                     System.out.println(Colors.RED + "Invalid option. Please try again." + Colors.RESET);
+                    promptToContinue();
             }
-        }while (option >1 && option <8);
+        }
     }
+
+    public static void displayAccountInfo(){
+        if (checkingAccount != null && savingAccount != null) {
+            System.out.println(Colors.GREEN + "Displaying both accounts:" + Colors.RESET);
+            checkingAccount.display();
+            savingAccount.display();
+        } else if (checkingAccount != null) {
+            System.out.println(Colors.GREEN + "Displaying Checking Account:" + Colors.RESET);
+            checkingAccount.display();
+        } else if (savingAccount != null) {
+            System.out.println(Colors.GREEN + "Displaying Saving Account:" + Colors.RESET);
+            savingAccount.display();
+        } else {
+            System.out.println(Colors.RED + "Neither Checking nor Saving account is created!" + Colors.RESET);
+        }
+    }
+
+
+
+//  ---------------------------------------------------------------------------------------------
+    private static void promptToContinue() {
+        CellStyle centerStyle = new CellStyle(CellStyle.HorizontalAlign.center);
+        Table t = new Table(1, BorderStyle.UNICODE_ROUND_BOX_WIDE, ShownBorders.ALL);
+        // Set column widths
+        t.setColumnWidth(0, 50, 70);
+
+        t.addCell(Colors.RED + "Press Enter to continue..." + Colors.RESET , centerStyle);
+        System.out.println(t.render());
+        scanner.nextLine();
+    }
+//  ------------------------------------------------------------------------------------------------
+
 //  _______________________________________________________________________________________________________
     class Colors{
         public static final String RESET = "\u001B[0m";
@@ -57,7 +90,7 @@ public class Main {
         public static final String YELLOW = "\u001B[33m";
     }
 //  ________________________________________________________________________________________________
-    public static void dispalyMenu(){
+    public static void displayMenu(){
         System.out.println(Colors.BLUE + "\n===== Online Banking System =====" + Colors.RESET);
         System.out.println("1. Create Account");
         System.out.println("2. Deposit Money");
@@ -68,4 +101,66 @@ public class Main {
         System.out.println("7. Exit");
         System.out.print(Colors.YELLOW + "Choose an option (1-7): " + Colors.RESET);
     }
+//    -------------------------------------------------------------------------------------
+
+    public static void createAccount(){
+        System.out.println(Colors.GREEN + "Creating Account" + Colors.RESET);
+        System.out.println("1. Checking Account");
+        System.out.println("2. Saving Account");
+        System.out.print("Choose an option: ");
+        int accountType = scanner.nextInt();
+        scanner.nextLine();
+
+        if(accountType == 1){
+            System.out.print("Enter username: ");
+            String userName = scanner.nextLine();
+            System.out.print("Enter date of birth (dd-mm-yyyy): ");
+            String dob = scanner.nextLine();
+            System.out.print("Enter gender: ");
+            String gender = scanner.nextLine();
+            System.out.print("Enter phone number (9 digits): ");
+            String phone = scanner.nextLine();
+
+            accountNumber=String.valueOf((int)(Math.random()*1000000));
+            checkingAccount = new CheckingAccount(userName, dob, gender, phone, accountNumber);
+        }else {
+            System.out.print("Enter username: ");
+            String userName = scanner.nextLine();
+            System.out.print("Enter date of birth (dd-mm-yyyy): ");
+            String dob = scanner.nextLine();
+            System.out.print("Enter gender: ");
+            String gender = scanner.nextLine();
+            System.out.print("Enter phone number (9 digits): ");
+            String phone = scanner.nextLine();
+
+            accountNumber = String.valueOf((int)(Math.random()*1000000));
+            savingAccount =  new SavingAccount(userName, dob, gender, phone, accountNumber);
+        }
+    }
+    public static void depositMoney(){
+        System.out.println("1. checking account: ");
+        System.out.println("2. saving account: ");
+        String accountType = scanner.nextLine();
+        if(accountType.equals("1")){
+            if(checkingAccount == null){
+                System.out.println("Don't have checking account!");
+            }else {
+                System.out.println("Enter money to deposit: ");
+                checkingAccount.deposit(scanner.nextInt());
+            }
+        }else {
+            if(savingAccount == null){
+                System.out.println("Don't have saving account!");
+            }else {
+                System.out.println("Enter money to deposit: ");
+                int amount = scanner.nextInt();
+                savingAccount.deposit(amount);
+            }
+        }
+    }
+
+
+
+
+
 }
